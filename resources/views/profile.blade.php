8e3cb7c8490f310747e59f2bdd90c74462274d3c
Vynix70 <dhioalfian@students.amikom.ac.id>
2026-04-10 08:36:35 +0700

    Create profile.blade.php

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Profil</title>
+
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-slate-100 flex items-center justify-center h-screen">
+    <div class="bg-white p-8 rounded-xl shadow-lg border border-slate-200 text-center max-w-sm w-full">
+        <h1 class="text-2xl font-bold text-slate-800 mb-2">Profil Saya</h1>
+        <p class="text-slate-500 mb-6">Nama: Dhio Alfiansyach Putra</p>
+        <p class="text-slate-500 mb-6">NIM: 24.12.3324</p>
+        <p class="text-slate-500 mb-6">Kelas: SI04</p>
+        <a href="/" class="inline-block bg-indigo-600 text-white font-semibold py-2 px-6 rounded-lg hover:bg-indigo-700 hover:shadow-md transition duration-300">Kembali ke Home</a>
+        <a href="/contact" class="inline-block bg-gray-600 text-white font-semibold py-2 px-6 rounded-lg hover:bg-gray-700 hover:shadow-md transition duration-300 mt-4">Hubungi Saya</a>
+        <a href="/bantuan" class="inline-block bg-yellow-600 text-white font-semibold py-2 px-6 rounded-lg hover:bg-yellow-700 hover:shadow-md transition duration-300 mt-4">Bantuan</a>
+        <a href="/katalog" class="inline-block bg-green-600 text-white font-semibold py-2 px-6 rounded-lg hover:bg-green-700 hover:shadow-md transition duration-300 mt-4">Lihat Katalog Event</a>
+    </div>
+    
+</body>
+</html>
