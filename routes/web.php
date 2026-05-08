644557c74c371fb4c1b9cb785d9e0a6dec5e354f
Vynix70 <dhioalfian@students.amikom.ac.id>
2026-05-08 08:08:45 +0700

    Tugas 4

--- routes/web.php
+++ routes/web.php
@@ -1,30 +1,33 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\EventController;
-use App\Http\Controllers\TicketController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\EventController as AdminEventController;
 use App\Http\Controllers\Admin\CategoriesController;
 use App\Http\Controllers\Admin\TransactionController;
-use Illuminate\Support\Facades\Route;
 
+// USER
+Route::get('/', [HomeController::class, 'index']);
 
-//user
+Route::get('/event-detail', [EventController::class, 'show']);
+Route::get('/checkout', [EventController::class, 'chekout'])->name('checkout');
 
-Route::get('/', [App\Http\Controllers\HomeController::class, 'index']);
-
-Route::get('/event-detail', [App\Http\Controllers\EventController::class, 'show']);
-Route::get('/checkout', [App\Http\Controllers\EventController::class, 'chekout'])->name('checkout');
-
-
+// ADMIN
 Route::redirect('/admin', '/admin/dashboard');
-//admin
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], function (){
 
-    Route::get('/dashboard', [DashboardController::class, 'index']);
-    Route::get('/events', [AdminEventController::class, 'index'])->name('events');
-    Route::get('/categories', [App\Http\Controllers\Admin\CategoriesController::class, 'index'])->name('categories');
-    Route::get('/transactions', [App\Http\Controllers\Admin\TransactionController::class, 'index'])->name('transactions');
-    
+Route::prefix('admin')->name('admin.')->group(function () {
+
+    Route::get('/dashboard', [DashboardController::class, 'index'])
+        ->name('dashboard');
+
+    Route::resource('events', AdminEventController::class);
+
+    Route::get('/categories', [CategoriesController::class, 'index'])
+        ->name('categories');
+
+    Route::get('/transactions', [TransactionController::class, 'index'])
+        ->name('transactions');
 });
