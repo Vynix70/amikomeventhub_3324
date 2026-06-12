722de80e51bfcf4165307c827f82016b819d192a
Vynix70 <dhioalfian@students.amikom.ac.id>
2026-06-12 07:57:00 +0700

    Praktikum-8(Login)

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -35,6 +35,6 @@ class AuthController extends Controller
         Auth::logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
-        return redirect('/');
+        return redirect()->route('admin.login'); // Arahkan kembali ke halaman login setelah logout
     }
 }
